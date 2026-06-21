331ca88f469541be41426488e43e2eafd0dd0218
wingthor9 <wingthor9@gmal.com>
2026-06-22 01:04:49 +0700

    jjj

--- src/lib/axiosInstance.ts
+++ src/lib/axiosInstance.ts
@@ -24,7 +24,7 @@ import axios from "axios"
 const axiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
   withCredentials: true,
-  headers: { "Content-Type": "application/json" },
+  // headers: { "Content-Type": "application/json" },
 })
 
 axiosInstance.interceptors.response.use(
